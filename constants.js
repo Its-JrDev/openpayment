44ec92b96816efe430136e1de27d7665aec3810d
leonela miranda <leonelamiranda141024@gmail.com>
2026-08-01 10:53:55 -0500

    Updated scripts in  package.json and setted keys; created folder for keys.

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
-export const SENDER_KEY_ID = "db57ba2d-b02a-4f88-913f-13943fbb447e"
-export const RECEIVER_KEY_ID = "3a773a5a-3948-4fd0-adeb-fb7b29197720"
-export const SENDER_WALLET = "https://ilp.interledger-test.dev/test-walletp"
-export const RECEIVER_WALLET = 'https://ilp.interledger-test.dev/wallet-usp';
-export const PRIVATE_SENDER_KEY_PATH = "./privatesender.key"
-export const PRIVATE_RECEIVER_KEY_PATH = "./privatereceiver.key"
+export const SENDER_KEY_ID = "MCowBQYDK2VwAyEA1Xu0MR3qBwreku8WeuZCh8WSg+XOVNhrCbwtjNESaeo="
+export const RECEIVER_KEY_ID = "MC4CAQAwBQYDK2VwBCIEIOoYkeWHZJWmcYzaAjFIk0ckT31YsVPDliHWnAJ7N8Pr"
+export const SENDER_WALLET = "https://ilp.interledger-test.dev/testing-send"
+export const RECEIVER_WALLET = 'https://ilp.interledger-test.dev/testing-reciver';
+export const PRIVATE_SENDER_KEY_PATH = "./keys/key-sender.key"
+export const PRIVATE_RECEIVER_KEY_PATH = "./keys/key-receiver.key"
